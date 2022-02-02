dd2d68f6678ce58adba0a923fa52e58d84a27713
Kurin, Alex <olexkurin@gmail.com>
2022-02-02 15:28:02 +0200

    algorithm changes

--- Programming-Lab13/Programming-Lab13.c
+++ Programming-Lab13/Programming-Lab13.c
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 
 #define MAX_SIZE 1000
+#define FILE_NAME "longest_words.txt"
 
 int main()
 {
@@ -9,18 +10,19 @@ int main()
 	scanf_s("%d", &n);
 	getchar();
 
-	FILE* stream;
-	fopen_s(&stream, "longest_words.txt", "w+");
+	FILE* fin;
+	fopen_s(&fin, FILE_NAME, "w");
 
-	for (int i = 0; i < n; i++)
+	for (int r = 0; r < n; r++)
 	{
 		char s[MAX_SIZE];
-		printf("\nEnter row %d\n", i + 1);
+		printf("\nEnter row %d:\n", r + 1);
 		gets_s(s, MAX_SIZE);
 
 		int longest_pos = 0;
 		int max_length = 0;
-		for (int i = 0; s[i] != '\0';)
+		int i = 0;
+		do
 		{
 			int length = 0;
 			for (; s[i] != '\0' && s[i] != ' '; length++)
@@ -32,25 +34,23 @@ int main()
 				max_length = length;
 				longest_pos = i - length;
 			}
-			if (s[i] == ' ')
-			{
-				i++;
-			}
-		}
-		fprintf(stream, "%.*s %d\n", max_length, s + longest_pos, max_length);
+		} while (s[i++] != '\0');
+		fprintf(fin, "%.*s %d\n", max_length, s + longest_pos, max_length);
 	}
-	rewind(stream);
+	fclose(fin);
 
+	FILE* fout;
+	fopen_s(&fout, FILE_NAME, "r");
 	printf("\nFile content:\n");
 	char s[MAX_SIZE];
-	while (!feof(stream))
+	while (!feof(fout))
 	{
-		fgets(s, MAX_SIZE, stream);
-		if (feof(stream))
+		fgets(s, MAX_SIZE, fout);
+		if (feof(fout))
 		{
 			break;
 		}
 		printf("%s", s);
 	}
-	fclose(stream);
+	fclose(fout);
 }
