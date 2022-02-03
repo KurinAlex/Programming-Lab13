fba5f95cb6aab11f5e136ff9e4ddff6ac62545c4
Kurin, Alex <olexkurin@gmail.com>
2022-02-03 22:47:41 +0200

    fixes

--- Programming-Lab13/Programming-Lab13.c
+++ Programming-Lab13/Programming-Lab13.c
@@ -19,23 +19,24 @@ int main()
 		printf("\nEnter row %d:\n", r + 1);
 		gets_s(s, MAX_SIZE);
 
-		int longest_pos = 0;
+		int longest_start_pos = 0;
 		int max_length = 0;
 		int i = 0;
 		do
 		{
 			int length = 0;
-			for (; s[i] != '\0' && s[i] != ' '; length++)
+			while (s[i] != '\0' && s[i] != ' ')
 			{
 				i++;
+				length++;
 			}
 			if (length > max_length)
 			{
 				max_length = length;
-				longest_pos = i - length;
+				longest_start_pos = i - length;
 			}
 		} while (s[i++] != '\0');
-		fprintf(fin, "%.*s %d\n", max_length, s + longest_pos, max_length);
+		fprintf(fin, "%.*s %d\n", max_length, s + longest_start_pos, max_length);
 	}
 	fclose(fin);
 
@@ -43,14 +44,12 @@ int main()
 	fopen_s(&fout, FILE_NAME, "r");
 	printf("\nFile content:\n");
 	char s[MAX_SIZE];
-	while (!feof(fout))
+	do
 	{
 		fgets(s, MAX_SIZE, fout);
-		if (feof(fout))
-		{
-			break;
-		}
 		printf("%s", s);
-	}
+	} while (!feof(fout));
 	fclose(fout);
+
+	getchar();
 }
